c4e34882a7d69de169e567a8e73098f77d18a986
xadamxk <xadamxk@gmail.com>
2015-09-28 00:40:38 -0500

    Windows GUI for USB Rubber Ducky
    Release 1

---
+++ RubberDuckyApp/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace RubberDuckyApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(116, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Make .bin";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(229, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Copy .bin";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(126, 26);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(104, 24);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Select HD:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.comboBox3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(339, 126);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Settings";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "be (French-Belgian)",
+            "br (Portuguese-Brazil)",
+            "ca (Candian)",
+            "ch (Swiss-German)",
+            "de (German)",
+            "dk (Danish)",
+            "es (Espanol)",
+            "fi (Finlad)",
+            "fr (French)",
+            "gb (United Kingdom)",
+            "hr (Croation)",
+            "it (Italian)",
+            "pt (Portuguese)",
+            "ru (Russian)",
+            "si (Slovenian)",
+            "sv (Swedish)",
+            "tr (Turkish)",
+            "us (United States)"});
+            this.comboBox2.Location = new System.Drawing.Point(126, 88);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(207, 24);
+            this.comboBox2.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 17);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Keyboard Layout:";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(236, 24);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 25);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Refresh Drives";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(126, 56);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(104, 24);
+            this.comboBox3.TabIndex = 12;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(6, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "MicroSD:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 144);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(339, 291);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Encoder";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 54);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(326, 230);
+            this.textBox1.TabIndex = 6;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(7, 25);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(104, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Save Payload";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 449);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(9, 449);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(244, 17);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "USB Rubber Ducky Tool By: xadamxk";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 475);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "USB Rubber Ducky Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ RubberDuckyApp/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Windows.Forms;
+
+/* To-Do List
+ * Look into grabbing existing scripts from wiki & implementing them via menu
+ * Firmware options?
+ * Other features?
+ */
+
+namespace RubberDuckyApp
+{
+    public partial class Form1 : Form
+    {
+        private string selectedDrive = ""; // ex C:\
+        private string selectedRemovableDrive = ""; // ex E:\
+        private string javaEXELocation = ""; 
+        private string duckyDirectory = ""; //C:\RubberDucky
+        private string javaHome = ""; // C:\RubberDucky\java.exe
+        private string encoderLocation = ""; // C:\RubberDucky\encoder.jar
+        bool microSDCheck;
+        private string[] keyboardStrings = {
+            "be", "br", "ca", "ch", "de", "dk",
+            "es", "fi", "fr", "gb", "hr", "it",
+            "pt", "ru", "si", "sv", "tr", "us"
+        };
+
+        const string encoderURL =
+            "https://github.com/midnitesnake/USB-Rubber-Ducky/blob/master/Encoder/encoder.jar?raw=true";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Auto Hide MicroSD Options
+            button1.Hide();
+            button2.Hide();
+            comboBox3.Hide();
+            comboBox2.SelectedIndex = 17;
+
+            refreshHDComboBox();
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Assign variable to selected drive
+            selectedDrive = comboBox1.SelectedItem.ToString();
+            duckyDirectory = selectedDrive + "RubberDucky";
+            
+            // Check selected drive for Rubber Ducky Directory
+            checkDirectoryExists();
+
+            // Check RubberDucky Directory for java
+            checkDirectoryJava();
+
+            // Check RubberDucky Directory for encoder
+            checkDirectoryEncoder();
+
+            // Check RubberDucky Directory for input.txt
+            checkDirectoryInputFile();
+
+            // Display MicroSD Options
+            displayMicroSD();
+
+            // Display Encoder Options
+            refreshEncoderElements();
+        }
+
+        private void displayMicroSD() // Auto
+        {
+            comboBox3.Show();
+
+            if (comboBox1.SelectedIndex != -1)
+            refreshMicroSDComboBox();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Make .bin Button
+            label5.Text = "Creating .bin";
+            string inputFile = "input.txt";
+            string outputFile = "inject.bin";
+            string languageProperty = keyboardStrings[comboBox2.SelectedIndex];
+            string encodeCommand = "cmd /c \"cd " + duckyDirectory + " & java -jar encoder.jar -i " + 
+                inputFile + " -o " + outputFile + " -l " + languageProperty + "\""; // cmd /c "cd c"\RubberDucky & java -jar encoder.jar -i input.txt -o inject.bin -l en"
+        
+                if (File.Exists(duckyDirectory + "\\" + outputFile))
+                    File.Delete(duckyDirectory + "\\" + outputFile);
+
+                Process process = new Process();
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = encodeCommand;
+                process.StartInfo = startInfo;
+                process.Start();
+            
+            // .bin created in label
+            label5.Text = ".bin Created In: " + duckyDirectory;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Copy .bin Button
+            refreshMicroSDComboBox();
+            if (comboBox3.SelectedIndex != -1) // Check title array for removable disks
+                    microSDCheck = true;
+
+            if ( microSDCheck)
+                File.Copy(duckyDirectory+"\\inject.bin",selectedRemovableDrive+"\\inject.bin", true);
+
+            // .bin copied to label
+            label5.Text = ".bin Copied To: " + comboBox3.SelectedItem;
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox3.SelectedIndex != -1)
+                selectedRemovableDrive = comboBox3.SelectedItem.ToString();
+        }
+
+        private void refreshHDComboBox()
+        {
+            // Assign drives to combobox1
+            comboBox1.Items.Clear();
+            // Credit to: http://stackoverflow.com/questions/623182/c-sharp-dropbox-of-drives
+            foreach (var Drives in Environment.GetLogicalDrives())
+            {
+                DriveInfo DriveInf = new DriveInfo(Drives);
+
+                if (DriveInf.IsReady)
+                {
+                    comboBox1.Items.Add(DriveInf.Name);
+                }
+            }
+
+            if (comboBox1.Items.Count > 0)
+                comboBox1.SelectedIndex = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Refresh Button
+            refreshHDComboBox();
+            refreshEncoderElements();
+            refreshMicroSDComboBox();
+
+            // Drives Refreshed label
+            label5.Text = "Drives Refreshed.";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // Save Payload Button
+            File.WriteAllText(duckyDirectory + "\\input.txt", textBox1.Text);
+
+            // Payload Saved Label
+            label5.Text = "Payload Saved.";
+        }
+
+        private void refreshMicroSDComboBox()
+        {
+            // Assign removable drives to combobox3
+            comboBox3.Items.Clear();
+            foreach (var Drives in Environment.GetLogicalDrives())
+            {
+                DriveInfo driveInf = new DriveInfo(Drives);
+
+                if (driveInf.IsReady)
+                    if (driveInf.DriveType.ToString().Contains("Removable"))
+                        comboBox3.Items.Add(driveInf.Name);
+            }
+
+            // Remove copies due to loop
+            object[] distinctItems = (from Object o in comboBox3.Items select o).Distinct().ToArray();
+            comboBox3.Items.Clear();
+            comboBox3.Items.AddRange(distinctItems);
+
+            if (comboBox3.Items.Count > 0)
+                comboBox3.SelectedIndex = 0;
+
+            if (comboBox3.Items.Count == 0)
+                MessageBox.Show(
+                    "No removable storage device was found.\n" +
+                    "Please insert the MicroSD card that will be used for your USB Rubber Ducky.\n" +
+                    "Click \"Refresh\" when ready.");
+        }
+
+        private void refreshEncoderElements()
+        {
+            if (comboBox1.SelectedIndex == -1)
+            {
+                button1.Hide();
+                button2.Hide();
+            }
+            else if (comboBox1.SelectedIndex != -1)
+            {
+                button1.Show();
+                button2.Show();
+            }
+        }
+
+        private void checkDirectoryExists()
+        {
+            if (!Directory.Exists(duckyDirectory))
+            {
+                DialogResult result = MessageBox.Show("The directory: \"" + duckyDirectory + "\"" + " does not exist.\nWould you like to create it for this application?",
+                    "NOTICE!",
+                    MessageBoxButtons.YesNoCancel,
+                    MessageBoxIcon.None,
+                    MessageBoxDefaultButton.Button3);
+
+                if (result == DialogResult.Yes) // If user said yes
+                {
+                    Directory.CreateDirectory(duckyDirectory);
+                    MessageBox.Show(duckyDirectory + "has been created!");
+                }
+            }
+        }
+
+        private void checkDirectoryJava()
+        {
+            string java32 = selectedDrive + "Program Files (x86)\\Java";
+            string java64 = selectedDrive + "Program Files\\Java";
+            javaHome = duckyDirectory + "\\java.exe";
+
+            if (!File.Exists(javaHome)) // If java.exe is NOT in \RubberDucky
+            {
+                if (Directory.Exists(java32)) // Then Java32 is installed
+                {
+                string[] java32subdirectories = Directory.GetDirectories(java32);
+                javaEXELocation = java32subdirectories.Last() + "\\bin\\java.exe"; // Selects most current version
+                File.Copy(javaEXELocation, javaHome);
+                }
+                else if (Directory.Exists(java64)) // Then Java64 is installed
+                {
+                    string[] java64subdirectories = Directory.GetDirectories(java64);
+                    javaEXELocation = java64subdirectories.Last() + "\\bin\\java.exe"; // Selects most current version
+                    File.Copy(javaEXELocation, javaHome);
+                }
+                else
+                {
+                    MessageBox.Show("No java.exe file was found.\n\n" +
+                                    "If you do not have Java installed, please download it from Oracle and re-run this program.\n\n" +
+                                    "If you installed Java to a non-default directory, " +
+                                    "please copy your java.exe file to:\n" + duckyDirectory + " and re-run this program.",
+                        "ERROR: No Java File Found!",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Exclamation);
+                }
+            }
+        }
+
+        private void checkDirectoryEncoder()
+        {
+            encoderLocation = duckyDirectory + "\\encoder.jar";
+            if (!File.Exists(encoderLocation))
+            {
+                // Download encoder.jar if not found
+                // Credits to: http://stackoverflow.com/questions/32223706/download-zipball-from-github-in-c-sharp
+                using (var client = new WebClient())
+                {
+                    client.Headers.Add("user-agent", "Anything");
+                    client.DownloadFile(encoderURL, encoderLocation);
+                }
+            }
+        }
+
+        private void checkDirectoryInputFile()
+        {
+            if (!File.Exists(duckyDirectory + "\\input.txt"))
+                File.CreateText(duckyDirectory + "\\input.txt");
+        }
+
+
+    }
+}
